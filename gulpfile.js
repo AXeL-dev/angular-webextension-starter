2b9cb5ac0f97d5bfa957f443047c23ebd3ce27ea
AXeL-dev <contact.axel.dev@gmail.com>
2019-12-30 21:12:51 +0100

    Update gulpfile.js
    Code refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,52 @@
+/**
+ * Angular WebExtension starter kit
+ * 
+ * @author      AXeL-dev <contact.axel.dev@gmail.com>
+ * @license     MPL-2.0
+ * @link        https://github.com/AXeL-dev/angular-webextension-starter
+ */
+
+/**
+ * Requirements
+ */
 const gulp = require('gulp');
 const shell = require('gulp-shell')
 const argv = require('yargs').argv;
 const file = require('gulp-file');
 const fs = require('fs');
 
-// Arguments
+/**
+ * Arguments
+ */
 const name = argv.name;
-const author = argv.author;
-const url = argv.url;
+const author = author === undefined ? '' : argv.author;
+const url = url === undefined ? '' : argv.url;
 
 // Arguments checks
 if (name === undefined) {
   console.log('Please provide --name argument');
   process.exit(1);
 }
-if (author === undefined) {
-  author = '';
-}
-if (url === undefined) {
-  url = '';
-}
 
-// Constants
-directory = 'output/' + name;
+/**
+ * Global constants
+ */
+const directory = 'output/' + name;
 
-// Functions
+/**
+ * Functions
+ */
 function getFileContent(file) {
   return fs.readFileSync(file, 'utf8');
 }
 
-// Tasks
+function getAbsolutePath(path) {
+  return directory + '/' + path.replace(/^\/+/g, '');
+}
+
+/**
+ * Tasks
+ */
 gulp.task('create-angular-project',
   shell.task('ng new ' + name + ' --defaults --routing=true --directory=' + directory)
 );
@@ -44,27 +61,27 @@ gulp.task('generate-settings-component',
 
 gulp.task('udpate-app-component', function() {
   return file('app.component.html', getFileContent('tpl/src/app/app.component.html'), { src: true })
-    .pipe(gulp.dest(directory + '/src/app'));
+    .pipe(gulp.dest(getAbsolutePath('/src/app')));
 });
 
 gulp.task('udpate-app-routing', function() {
   return file('app-routing.module.ts', getFileContent('tpl/src/app/app-routing.module.ts'), { src: true })
-    .pipe(gulp.dest(directory + '/src/app'));
+    .pipe(gulp.dest(getAbsolutePath('/src/app')));
 });
 
 gulp.task('add-guard-service', function() {
   return file('guard.service.ts', getFileContent('tpl/src/app/services/guard.service.ts'), { src: true })
-    .pipe(gulp.dest(directory + '/src/app/services'));
+    .pipe(gulp.dest(getAbsolutePath('/src/app/services')));
 });
 
 gulp.task('add-assets', function() {
   return file('icon.png', getFileContent('tpl/src/assets/images/icon.png'), { src: true })
-    .pipe(gulp.dest(directory + '/src/assets/images'));
+    .pipe(gulp.dest(getAbsolutePath('/src/assets/images')));
 });
 
 gulp.task('add-locales', function() {
   return file('messages.json', getFileContent('tpl/src/_locales/en/messages.json'), { src: true })
-    .pipe(gulp.dest(directory + '/src/_locales/en'));
+    .pipe(gulp.dest(getAbsolutePath('/src/_locales/en')));
 });
 
 gulp.task('add-manifest', function() {
@@ -72,16 +89,16 @@ gulp.task('add-manifest', function() {
   const newContent = content.replace('${author}', author).replace('${url}', url);
 
   return file('manifest.json', newContent, { src: true })
-    .pipe(gulp.dest(directory + '/src'));
+    .pipe(gulp.dest(getAbsolutePath('/src')));
 });
 
 gulp.task('add-nojekyll', function() {
   return file('.nojekyll', getFileContent('tpl/src/.nojekyll'), { src: true })
-    .pipe(gulp.dest(directory + '/src'));
+    .pipe(gulp.dest(getAbsolutePath('/src')));
 });
 
 gulp.task('update-angular.json', function() {
-  const content = getFileContent(directory + '/angular.json');
+  const content = getFileContent(getAbsolutePath('/angular.json'));
   const newContent = content.replace('"src/assets"', `"src/assets",
               "src/_locales",
               "src/manifest.json",
@@ -92,7 +109,7 @@ gulp.task('update-angular.json', function() {
 });
 
 gulp.task('update-package.json', function() {
-  const content = getFileContent(directory + '/package.json');
+  const content = getFileContent(getAbsolutePath('/package.json'));
   const newContent = content.replace('"build": "ng build",', `"build": "ng build --aot --prod --sourceMap=false --outputHashing=none",
     "package": "web-ext build --source-dir=dist/` + name + ` --artifacts-dir=.",
     "ghbuild": "ng build --prod --base-href \\"/` + name + `/\\"",
