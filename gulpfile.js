eca09fcee7fa95848b627eee1e081b2c70a3ccd1
AXeL-dev <contact.axel.dev@gmail.com>
2020-01-12 19:50:34 +0100

    Update udpate-popup-component task

--- gulpfile.js
+++ gulpfile.js
@@ -70,10 +70,19 @@ gulp.task('generate-settings-component',
   shell.task(`cd ${directory} && ng g c components/settings`)
 );
 
-gulp.task('udpate-popup-component', function() {
-  return file('popup.component.html', getFileContent(getExtensionPath('/src/app/app.component.html')), { src: true })
-    .pipe(gulp.dest(getExtensionPath('/src/app/components/popup')));
-});
+gulp.task('udpate-popup-component', gulp.series(
+  function() {
+    return file('popup.component.html', getFileContent('tpl/src/app/components/popup/popup.component.html'), { src: true })
+      .pipe(gulp.dest(getExtensionPath('/src/app/components/popup')));
+  },
+  function() {
+    const content = getFileContent('tpl/src/app/components/popup/popup.component.ts');
+    const newContent = content.replace('${title}', name);
+
+    return file('popup.component.ts', newContent, { src: true })
+      .pipe(gulp.dest(getExtensionPath('/src/app/components/popup')));
+  }
+));
 
 gulp.task('udpate-app-component', function() {
   return file('app.component.html', getFileContent('tpl/src/app/app.component.html'), { src: true })
---
+++ tpl/src/app/components/popup/popup.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popup',
+  templateUrl: './popup.component.html',
+  styleUrls: ['./popup.component.css']
+})
+export class PopupComponent implements OnInit {
+  title = '${title}';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
